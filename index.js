f43c885076f4a6d88cbbef6ab7536cb4331530a9
kimjin0 <kimjin0@naver.com>
2020-05-14 16:01:17 +0900

    2.7 DOM / If else / Function practice part Two

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const title = document.querySelector("#title");
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
-
-title.innerHTML = "Hello~~~";
+console.dir(title);
+const CLICKED_CLASS = "checked";
 
 function handleClick() {
-    const currentColor = title.style.color;
-    if (currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+    if (!hasClass) {
+        title.classList.add(CLICKED_CLASS);
     } else {
-        title.style.color = BASE_COLOR;
+        title.classList.remove(CLICKED_CLASS);
     }
 }
 
