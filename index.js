fc8e3420f05a1d7de6f138b911bfb27dd8db3647
kimjin0 <kimjin0@naver.com>
2020-05-14 16:13:13 +0900

    toogle 적용

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ console.dir(title);
 const CLICKED_CLASS = "checked";
 
 function handleClick() {
-    const hasClass = title.classList.contains(CLICKED_CLASS);
-    if (!hasClass) {
-        title.classList.add(CLICKED_CLASS);
-    } else {
-        title.classList.remove(CLICKED_CLASS);
-    }
+    title.classList.toggle(CLICKED_CLASS);
+    // toggle는 풀어서 작성하면 아래와 같다....
+
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if (!hasClass) {
+    //     title.classList.add(CLICKED_CLASS);
+    // } else {
+    //     title.classList.remove(CLICKED_CLASS);
+    // }
 }
 
 function init() {
