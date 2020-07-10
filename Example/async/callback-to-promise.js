ceb873bf6e4c84d1673f2b1d596942a468e590cb
kimjin0 <pkimjin0q@gmail.com>
2020-07-10 22:48:34 +0900

    callback.js , 콜백지옥의 코드를 Promise 이용해서 깔끔하게 해결....

--- Example/async/callback-to-promise.js
+++ Example/async/callback-to-promise.js
@@ -1,72 +1,65 @@
 "use strict";
 
-//callback.js 코드를 promise 코드화...
+// callback.js , 콜백지옥의 코드를 Promise 이용해서 깔끔하게 해결....
 
-console.log("1");
-setTimeout(() => console.log("2"), 1000);
-console.log("3");
-
-//Synchronous(동기) callback
-printCall(() => console.log("hello"));
-function printCall(print) {
-    print();
-}
-//Asynchronous(비동기) callback
-printWithDelay(() => console.log("async callback"), 2000);
-function printWithDelay(print, timeout) {
-    setTimeout(print, timeout);
-}
-
-// Callback Hell example (콜백지옥 체험하기)
-//1: 사용자에게 아이디, 비번 입력받음
-//2: 로그인 성공이면 사용자 역활을 받음
 class UserStorage {
-    loginUser(id, password, onSuccess, onError) {
-        setTimeout(() => {
-            if (
-                (id === "kimjin0" && password === "qwe123") ||
-                (id === "coder" && password === "academy")
-            ) {
-                onSuccess(id);
-            } else {
-                onError(new Error("not found"));
-            }
-        }, 2000);
-    }
+	loginUser(id, password) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				if (
+					(id === "kimjin0" && password === "qwe123") ||
+					(id === "coder" && password === "academy")
+				) {
+					resolve(id);
+				} else {
+					reject(new Error("not found"));
+				}
+			}, 2000);
+		});
+	}
 
-    getRoles(user, onSuccess, onError) {
-        setTimeout(() => {
-            if (user === "kimjin0") {
-                onSuccess({ name: "kimjin0", role: "admin" });
-            } else {
-                onError(new Error("no access"));
-            }
-        }, 1000);
-    }
+	getRoles(user) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				if (user === "kimjin0") {
+					resolve({ name: "kimjin0", role: "admin" });
+				} else {
+					reject(new Error("no access"));
+				}
+			}, 1000);
+		});
+	}
 }
 
 const userStorage = new UserStorage();
 const id = prompt("enter your id");
 const password = prompt("enter your password");
-userStorage.loginUser(
-    id,
-    password,
-    (user) => {
-        userStorage.getRoles(
-            user,
-            (userWithRole) => {
-                alert(
-                    `Hello ${userWithRole.name}, you have ${userWithRole.role} role`
-                );
-            },
-            (error) => {
-                console.log(error);
-            }
-        );
-    },
-    (error) => {
-        console.log(error);
-    }
-);
 
-https://youtu.be/JB_yU6Oe2eE?list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2
+userStorage //
+	.loginUser(id, password)
+	.then(userStorage.getRoles)
+	.then((user) => alert(`Hello ${user.name}, you have ${user.role} role`))
+	.catch(console.log);
+
+// userStorage.loginUser(
+// 	id,
+// 	password,
+// 	(user) => {
+// 		userStorage.getRoles(
+// 			user,
+// 			(userWithRole) => {
+// 				alert(
+// 					`Hello ${userWithRole.name}, you have ${userWithRole.role} role`
+// 				);
+// 			},
+// 			(error) => {
+// 				console.log(error);
+// 			}
+// 		);
+// 	},
+// 	(error) => {
+// 		console.log(error);
+// 	}
+// );
+
+// https://youtu.be/JB_yU6Oe2eE?list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2
