ab0ab4e027f5fe9154838abf706a826d5d078ff5
kimjin0 <pkimjin0q@gmail.com>
2020-07-16 08:49:07 +0900

    음.... 아직 async, await 잘 모르겠다... 내일 다시 해보자

--- Example/ext/async.js
+++ Example/ext/async.js
@@ -8,6 +8,56 @@ async function getMenu1() {
 }
 
 // getMenu1().then(console.log);
-getMenu1().then((menu) => {
-    console.log(menu);
-});
+// getMenu1().then((menu) => {
+//     console.log(menu);
+// });
+
+let userConfirm = function (id, password) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if ((id === "kimjin0" && password === "qwe123") || (id === "coder" && password === "academy")) {
+                resolve(id);
+            } else {
+                reject(new Error("not found"));
+            }
+        }, 2000);
+    });
+};
+
+async function getUser(id, password) {
+    let user = await userConfirm(id, password);
+    return user;
+}
+
+// const id = prompt("enter your id");
+// const password = prompt("enter your password");
+const user = getUser("kimjin0", "qwe123");
+console.log(user);
+// class UserStorage {
+// 	loginUser(id, password) {
+// 		return new Promise((resolve, reject) => {
+// 			setTimeout(() => {
+// 				if (
+// 					(id === "kimjin0" && password === "qwe123") ||
+// 					(id === "coder" && password === "academy")
+// 				) {
+// 					resolve(id);
+// 				} else {
+// 					reject(new Error("not found"));
+// 				}
+// 			}, 2000);
+// 		});
+// 	}
+
+// 	getRoles(user) {
+// 		return new Promise((resolve, reject) => {
+// 			setTimeout(() => {
+// 				if (user === "kimjin0") {
+// 					resolve({ name: "kimjin0", role: "admin" });
+// 				} else {
+// 					reject(new Error("no access"));
+// 				}
+// 			}, 1000);
+// 		});
+// 	}
+// }
