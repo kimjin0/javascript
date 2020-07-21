29c0298df3d7d78d1e4475bed1367adddb67342f
kimjin0 <pkimjin0q@gmail.com>
2020-07-21 13:47:40 +0900

    callback-to-promie 따라하기

--- Example/ext/callback.js
+++ Example/ext/callback.js
@@ -3,6 +3,8 @@
 //1: 사용자에게 아이디, 비번 입력받음
 //2: 로그인 성공이면 사용자 역활을 받음
 
+/*
+######## callBack ########
 class UserStorage {
     getUser(id, pwd, onSuccess, onError) {
         setTimeout(() => {
@@ -46,3 +48,40 @@ userStorage.getUser(
         console.log(error);
     }
 );
+*/
+
+// ######## callBack to Promise ########
+class UserStorage {
+    getUser(id, pwd) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if ((id === "kimjin0" && pwd === "qwe123") || (id === "coder" && pwd === "academy")) {
+                    resolve(id);
+                } else {
+                    reject(new Error("not found"));
+                }
+            }, 1000);
+        });
+    }
+
+    getRole(user) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (user === "kimjin0") {
+                    resolve({ name: "kimjin0", role: "admin" });
+                } else {
+                    reject(new Error("no access"));
+                }
+            }, 1000);
+        });
+    }
+}
+
+const userStorage = new UserStorage();
+const id = "kimjin0";
+const pwd = "qwe123";
+
+userStorage //
+    .getUser(id, pwd)
+    .then(userStorage.getRole)
+    .then(console.log);
