292052ce11ec94e1286fbc65c73eef0ea40c809a
kimjin0 <pkimjin0q@gmail.com>
2020-07-25 23:49:29 +0900

    math.trunc 이해가 부족함.

---
+++ Example/ext/script.js
@@ -0,0 +1,5 @@
+const ITEM_COLOR = ["blue", "yellow", "pink"];
+console.log(Math.random());
+console.log(Math.random() * ITEM_COLOR.length);
+let type = Math.trunc(Math.random() * ITEM_COLOR.length);
+console.log(type);
