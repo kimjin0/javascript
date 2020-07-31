b699db81638bcd160b59bfc07506b076618625f6
kimjin0 <pkimjin0q@gmail.com>
2020-07-31 21:10:29 +0900

    class에 상속까지 공부 했으니 코딩해보길...

--- Example/class.js
+++ Example/class.js
@@ -14,3 +14,5 @@ console.log(kimjin0.age);
 kimjin0.speak();
 
 // https://youtu.be/_DLhUBWsRtw?list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&t=614
+// class에 상속까지 공부 했으니 코딩해보길...
+// https://www.youtube.com/watch?v=_DLhUBWsRtw&feature=youtu.be&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&t=614
