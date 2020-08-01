5c1d2d246d2aaf95417912621deda6361aa03a44
kimjin0 <pkimjin0q@gmail.com>
2020-08-01 22:49:00 +0900

    영상시청으로 커밋

--- Example/class.js
+++ Example/class.js
@@ -14,5 +14,6 @@ console.log(kimjin0.age);
 kimjin0.speak();
 
 // https://youtu.be/_DLhUBWsRtw?list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&t=614
-// class에 상속까지 공부 했으니 코딩해보길...
+// class에 상속까지 공부
 // https://www.youtube.com/watch?v=_DLhUBWsRtw&feature=youtu.be&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&t=614
+https://www.youtube.com/watch?v=_DLhUBWsRtw&feature=youtu.be&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&t=614
