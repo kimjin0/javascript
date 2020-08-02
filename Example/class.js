5f1b453d2d524df80dddcfc6cc513d42e7db88bb
kimjin0 <pkimjin0q@gmail.com>
2020-08-02 23:03:48 +0900

    쩝~~

--- Example/class.js
+++ Example/class.js
@@ -16,4 +16,4 @@ kimjin0.speak();
 // https://youtu.be/_DLhUBWsRtw?list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&t=614
 // class에 상속까지 공부
 // https://www.youtube.com/watch?v=_DLhUBWsRtw&feature=youtu.be&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&t=614
-https://www.youtube.com/watch?v=_DLhUBWsRtw&feature=youtu.be&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&t=614
+// https://www.youtube.com/watch?v=_DLhUBWsRtw&feature=youtu.be&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&t=614
