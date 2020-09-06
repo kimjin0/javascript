2d147ccea0036300168fadb5ceab9c01bedb7043
kimjin0 <pkimjin0q@gmail.com>
2020-09-06 15:10:24 +0900

    https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Classes 공부중...

--- class/class.js
+++ class/class.js
@@ -1,33 +1,21 @@
-class Shape {
-    constructor(width, height, color) {
+// Prototype methods
+class Rectangle {
+    constructor(width, height) {
         this.width = width;
         this.height = height;
-        this.color = color;
     }
 
-    draw() {
-        console.log(`drawing ${this.color} color!!`);
+    // Getter
+    get area() {
+        return this.calcArea();
     }
 
-    getArea() {
+    //Method
+    calcArea() {
         return this.width * this.height;
     }
 }
 
-class Rectangle extends Shape {}
-rectangle = new Rectangle(20, 20, "blue");
-rectangle.draw();
-console.log(rectangle.getArea());
-
-class Triangle extends Shape {
-    draw() {
-        super.draw;
-        console.log(`🍕`);
-    }
-    getArea() {
-        return (this.width * this.height) / 2;
-    }
-}
-triangle = new Triangle(20, 20, "red");
-triangle.draw();
-console.log(triangle.getArea());
+const square = new Rectangle(10, 10);
+console.log(square.area);
+console.log(square.calcArea());
