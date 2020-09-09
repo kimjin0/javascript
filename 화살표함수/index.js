4dbb4e35cedf37e6f2eff35b643a6a9d58f87c6c
kimjin0 <pkimjin0q@gmail.com>
2020-09-10 08:38:04 +0900

    화살표 함수 > 바인딩 되지 않은 arguments

--- 화살표함수/index.js
+++ 화살표함수/index.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 // MDN : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/%EC%95%A0%EB%A1%9C%EC%9A%B0_%ED%8E%91%EC%85%98
 
 // 매개변수 목록 내 구조분해할당도 지원됨.
@@ -8,6 +8,10 @@ console.log(f());
 // 짧은 함수
 const items = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium'];
 
+// 보통의 함수
+items.map(function (item) {
+    console.log('보통의 함수 : ' + item.length);
+});
 //잛아짐 1단계
 console.log(items.map((item) => item.length));
 
@@ -25,3 +29,22 @@ console.log(rst); // [8, 6, 7, 9]
 // 리터럴 이름은 우리가 해당 객체에서 꺼내오고 싶은 속성이름 자체로 사용됩니다.
 let rst1 = items.map(({ length }) => length);
 console.log(rst1); // [8, 6, 7, 9]
+
+console.log('### 바인딩 되지 않은 arguments ###');
+// 화살표 함수는 arguments 객체를 바인드 하지 않습니다.
+// 때문에, arguments는  그저 둘러싸는 범위(scope) 내 이름에 대한 참조입니다.
+var arguments = [1, 2, 3];
+var arr = (n) => console.log(arguments[0]);
+arr(4); // 1
+
+var arr2 = function (n) {
+    console.log(arguments[0]); //arguments가 바인딩되어 넘겨준 4가 나옴. 이게 화살표함수에서 안된다는 뜻
+};
+arr2(4); //4
+
+// function foo(n) {
+//     var f = () => arguments[0] + n; // foo's implicit arguments binding. arguments[0] is n
+//     return f();
+// }
+
+// console.log(foo(4)); // 2
