7e7c2792aa33d24d7e03cc2b3bd1714009f94ae6
kimjin0 <pkimjin0q@gmail.com>
2020-09-14 08:39:05 +0900

    배열 추가, 삭제

--- Array/index.js
+++ Array/index.js
@@ -1,13 +1,12 @@
 let fruits = ['사과', '바나나'];
 
-//배열 끝에 항목 추가하기
-fruits.push('딸기'); //["사과", "바나나", "딸기"]
-
-// 배열 끝에서부터 항목 제거하기
-fruits.pop(); //["사과", "바나나"]
-
-//배열 앞에서부터 항목 제거하기
-fruits.shift(); //["바나나"]
-
 //배열의 항목들을 순환하며 처리하기
-fruits.forEach((item, index, array) => console.log(item, index, array));
+fruits.forEach((item, index, array) => console.log(item, index));
+
+let myList = [1, 2, 3];
+myList.push('끝에추가');
+myList.unshift('앞에추가');
+myList.shift(); //앞에저게
+myList.pop(); //뒤에제거
+
+console.log(myList);
