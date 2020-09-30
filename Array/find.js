b4fee05ff9f39b137ff587dffb7db172cdeea660
kimjin0 <pkimjin0q@gmail.com>
2020-09-30 20:43:25 +0900

    find 복습

--- Array/find.js
+++ Array/find.js
@@ -1,7 +1,7 @@
 // find() 메서드는 주어진 판별 함수를 만족하는 첫 번째 요소의 값을 반환합니다. 그런 요소가 없다면 undefined를 반환합니다.
 
 const array1 = [5, 12, 8, 130, 44];
-const found = array1.find((element) => element > 10);
+const found = array1.find((element) => element > 6);
 console.log(found); //12
 
 // ## 속성 중 하나를 사용하여 배열에서 객체 찾기
@@ -11,5 +11,6 @@ var inventory = [
     { name: "cherries", quantity: 5 },
 ];
 
-let result = inventory.find((fruit) => fruit.name === "bananas");
+// let result = inventory.find((fruit) => fruit.name === "bananas");
+let result = inventory.find((fruit) => fruit.name === "cherries");
 console.log(result);
