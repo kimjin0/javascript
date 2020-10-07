5f2ce3f7f2b408c7f76533c64c28c738fe3646bc
kimjin0 <pkimjin0q@gmail.com>
2020-10-07 16:20:29 +0900

    새선국 부서 홈페이지 관리권한 연습

--- Array/find.js
+++ Array/find.js
@@ -6,11 +6,22 @@ console.log(found); //12
 
 // ## 속성 중 하나를 사용하여 배열에서 객체 찾기
 var inventory = [
-    { name: "apples", quantity: 2 },
-    { name: "bananas", quantity: 0 },
-    { name: "cherries", quantity: 5 },
+    { name: 'apples', quantity: 2 },
+    { name: 'bananas', quantity: 0 },
+    { name: 'cherries', quantity: 5 },
 ];
 
 // let result = inventory.find((fruit) => fruit.name === "bananas");
-let result = inventory.find((fruit) => fruit.name === "cherries");
+let result = inventory.find((fruit) => fruit.name === 'cherries');
 console.log(result);
+
+var auth = [
+    { key: '1차작사심사', value: '심사자' },
+    { key: '2차작사심사', value: '관리자' },
+    { key: '합창심사', value: '심사자,관리자' },
+    { key: '작곡심사', value: '관리자' },
+];
+
+let result2 = auth.find((item) => item.key === '합창심사');
+console.log(result2);
+console.log(result2.value.split(','));
