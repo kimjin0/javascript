dcc0627ebad59f4af9f96edc0890331dccd03ab0
kimjin0 <pkimjin0q@gmail.com>
2022-03-10 14:01:00 +0900

    test commit

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "checked";
 
 function handleClick() {
-    title.classList.toggle(CLICKED_CLASS);
-    // toggle는 풀어서 작성하면 아래와 같다....
+  title.classList.toggle(CLICKED_CLASS);
+  // toggle는 풀어서 작성하면 아래와 같다....
 
-    // const hasClass = title.classList.contains(CLICKED_CLASS);
-    // if (!hasClass) {
-    //     title.classList.add(CLICKED_CLASS);
-    // } else {
-    //     title.classList.remove(CLICKED_CLASS);
-    // }
+  // const hasClass = title.classList.contains(CLICKED_CLASS);
+  // if (!hasClass) {
+  //     title.classList.add(CLICKED_CLASS);
+  // } else {
+  //     title.classList.remove(CLICKED_CLASS);
+  // ////}
 }
 
 function init() {
-    title.addEventListener("click", handleClick);
+  title.addEventListener("click", handleClick);
 }
 
 init();
